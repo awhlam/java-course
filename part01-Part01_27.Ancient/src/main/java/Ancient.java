8a59cc0d918bbd423c2d04a99b39f50b28ddb6fa
Andrew Lam <alam1324@gmail.com>
2022-12-31 15:33:07 -0800

    I Part 1 24-34

--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -6,6 +6,10 @@ public class Ancient {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give a year:");
+        int number = Integer.valueOf(scan.nextLine());
+        if (number < 2015) {
+            System.out.println("Ancient history!");
+        }
     }
 }
--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -5,7 +5,13 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here 
+        System.out.println("Password?");
+        String input = scan.nextLine();
+        String password = "Caput Draconis";
+        if (input.equals(password)) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
