a7a1f29f0d34380edc0c2471de124f070e9bcc83
Andrew Lam <anlam@zoox.com>
2023-05-06 18:25:36 -0700

    part02-Part02_07-08

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,6 +5,16 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+            } else if(number == 0) {
+                return;
+            } else {
+                System.out.println(number * number);
+            }
+        }
     }
 }
